8028505bf6aba6aed5d01259c1b9cbdb5c917a75
tian830426 <105027318+tian830426@users.noreply.github.com>
2022-09-09 02:16:44 +0800

    123
    git

---
+++ all.js
@@ -0,0 +1,34 @@
+
+
+$(function () {
+
+var $win = $(window);
+
+var $backToTop = $('.js-back-to-top');
+
+// 當用戶滾動到離頂部100像素時，展示回到頂部按鈕
+
+$win.scroll(function () {
+
+if ($win.scrollTop() > 100) {
+
+$backToTop.show();
+
+} else {
+
+$backToTop.hide();
+
+}
+
+});
+
+// 當用戶點擊按鈕時，通過動畫效果返回頭部
+
+$backToTop.click(function () {
+
+$('html, body').animate({scrollTop: 0}, 200);
+
+});
+
+});
+
